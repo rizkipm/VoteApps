165fb7ccce5ee4d1f41a77beeb04f5dbd544d373
Programmer Muslim <rizki@imastudio.co.id>
2017-10-16 00:42:35 +0700

    fix bug
    add model dokumentasi

---
+++ app/src/main/java/imastudio/rizki/com/polingapps/model/ModelDokumentasi.java
@@ -0,0 +1,42 @@
+package imastudio.rizki.com.polingapps.model;
+
+/**
+ * Created by rizkisyaputra on 10/16/17.
+ */
+
+public class ModelDokumentasi {
+
+    private String id_dok, nama_dok, foto_dok, point_dok;
+
+    public String getId_dok() {
+        return id_dok;
+    }
+
+    public void setId_dok(String id_dok) {
+        this.id_dok = id_dok;
+    }
+
+    public String getNama_dok() {
+        return nama_dok;
+    }
+
+    public void setNama_dok(String nama_dok) {
+        this.nama_dok = nama_dok;
+    }
+
+    public String getFoto_dok() {
+        return foto_dok;
+    }
+
+    public void setFoto_dok(String foto_dok) {
+        this.foto_dok = foto_dok;
+    }
+
+    public String getPoint_dok() {
+        return point_dok;
+    }
+
+    public void setPoint_dok(String point_dok) {
+        this.point_dok = point_dok;
+    }
+}
