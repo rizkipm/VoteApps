4acdb8d6e36fb6d279b0c5f0b7274688a10e4e77
Programmer Muslim <rizki@imastudio.co.id>
2017-10-16 00:08:45 +0700

    fix bug
    add menu galeri

--- app/src/main/java/imastudio/rizki/com/polingapps/InputDataPoling.java
+++ app/src/main/java/imastudio/rizki/com/polingapps/InputDataPoling.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
@@ -192,5 +194,28 @@ public class InputDataPoling extends AppCompatActivity {
 
         }
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_galeri, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.show_galeri) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
